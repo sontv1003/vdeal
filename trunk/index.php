b5c82f5bc7543a55c01a3a0ae49906f4dcec0346
transontt@gmail.com <transontt@gmail.com@1898dba5-7468-a263-289e-c80d6de817ac>
2012-06-10 03:27:01 +0000

    Thieu common trong duong dan  include 2 file JS o duoi cuoi file login.php

--- trunk/index.php
+++ trunk/index.php
@@ -1,10 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php 
 require_once('includes/config.php'); 
 require_once('includes/util.php'); 
 $Util = new Util();
-
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
